81513576a8c518f688fac297e0bfa3ca1acd20ff
Vishnu Razdan <vishnu.razdan@gmail.com>
2015-05-09 17:34:27 -0400

    Revert "Fixed scheduling"
    This reverts commit 317e2e4acf6eba499440a37cd64a1221a550bc5c.

--- 418Cache/418Cache/CacheController.cpp
+++ 418Cache/418Cache/CacheController.cpp
@@ -19,26 +19,6 @@ CacheController::~CacheController(void)
 {
 }
 
-bool jobNoRequireBus(CacheJob* currJob, Cache* cache)
-{
-	int jobTID = (*currJob).getThreadId();
-	unsigned long long jobAddr = (*currJob).getAddress();
-	int whichset;
-	int tag;
-	(*cache).decode_address(jobAddr, &whichset, &tag);
-	CacheSet* currSet = (*cache).localCache[whichset];
-	if (!(*currSet).hasLine(tag))
-		return false;
-	CacheLine* currLine = (*currSet).getLine(tag);
-	if ((*currJob).isWrite())
-	{
-		if (!((*currLine).getState() == CacheLine::modified) && 
-			!((*currLine).getState() == CacheLine::exclusive))
-			return false;
-	}
-	return true;
-}
-
 bool queuesEmpty(std::vector<Cache*> caches){
 	bool allEmpty = true;
 	for(int i = 0; i < caches.size(); i++){
@@ -143,12 +123,12 @@ int main(int argc, char* argv[]){
 		//time must first increment for the constants
 		constants.tick();
 		//then call for all the caches
-		CacheJob* currJob = outstandingRequests.front();
-		int currThread = (*currJob).getThreadId();
-		if (noJobs(caches) || jobNoRequireBus(currJob, caches[currThread]))
+		if(noJobs(caches))
 		{
 			printf("at cycle %llu we process a new job \n", constants.getCycle());
+			CacheJob* currJob = outstandingRequests.front();
 			outstandingRequests.pop();
+			int currThread = (*currJob).getThreadId();
 			((*(caches[currThread])).pendingJobs).push(currJob);
 		}
 		for(int j = 0; j < numProcessors; j++){
@@ -156,6 +136,7 @@ int main(int argc, char* argv[]){
 		}
 		//then call the bus manager
 		(*bus).tick();
+
 	}
 
 
